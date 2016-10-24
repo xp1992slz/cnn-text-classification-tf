1a4b505c0ccd8796247051a35b492b9947410371
jihopark <lastspurt17@gmail.com>
2016-10-24 17:14:36 +0800

    updated eval method to have interactive mode

--- eval.py
+++ eval.py
@@ -20,6 +20,7 @@ tf.flags.DEFINE_boolean("eval_train", False, "Evaluate on all training data")
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+tf.flags.DEFINE_boolean("interactive_mode", True, "Make it interactive")
 
 
 FLAGS = tf.flags.FLAGS
@@ -34,8 +35,8 @@ if FLAGS.eval_train:
     x_raw, y_test = data_helpers.load_data_and_labels()
     y_test = np.argmax(y_test, axis=1)
 else:
-    x_raw = ["a masterpiece four years in the making", "everything is off."]
-    y_test = [1, 0]
+    x_raw = ["a masterpiece four years in the making"]
+    y_test = None
 
 # Map data into vocabulary
 vocab_path = os.path.join(FLAGS.checkpoint_dir, "..", "vocab")
@@ -46,7 +47,21 @@ print("\nEvaluating...\n")
 
 # Evaluation
 # ==================================================
-checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+
+def evaluate(x_test):
+    # Generate batches for one epoch
+    batches = data_helpers.batch_iter(list(x_test), FLAGS.batch_size, 1, shuffle=False)
+
+    # Collect the predictions here
+    all_predictions = []
+
+    for x_test_batch in batches:
+        batch_predictions = sess.run(predictions, {input_x: x_test_batch, dropout_keep_prob: 1.0})
+        all_predictions = np.concatenate([all_predictions, batch_predictions])
+    return all_predictions
+
+checkpoint_file = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir).model_checkpoint_path
+
 graph = tf.Graph()
 with graph.as_default():
     session_conf = tf.ConfigProto(
@@ -66,18 +81,20 @@ with graph.as_default():
         # Tensors we want to evaluate
         predictions = graph.get_operation_by_name("output/predictions").outputs[0]
 
-        # Generate batches for one epoch
-        batches = data_helpers.batch_iter(list(x_test), FLAGS.batch_size, 1, shuffle=False)
+        if FLAGS.interactive_mode:
+            while True:
+                x = raw_input("Please enter something (enter exit to quit):")
+                if x == "exit":
+                    exit()
 
-        # Collect the predictions here
-        all_predictions = []
+                prediction = evaluate(np.array(list(vocab_processor.transform([x]))))
+                print "\nprediction is %s" % ("positive" if prediction[0] == 1 else "negative")
 
-        for x_test_batch in batches:
-            batch_predictions = sess.run(predictions, {input_x: x_test_batch, dropout_keep_prob: 1.0})
-            all_predictions = np.concatenate([all_predictions, batch_predictions])
+        all_predictions = evaluate(x_test)
 
 # Print accuracy if y_test is defined
 if y_test is not None:
+    print all_predictions
     correct_predictions = float(sum(all_predictions == y_test))
     print("Total number of test examples: {}".format(len(y_test)))
     print("Accuracy: {:g}".format(correct_predictions/float(len(y_test))))
